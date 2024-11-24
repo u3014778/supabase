a1c01c322a7add48bd381341c4ea310dff86f3ae
u3014778 <33469959+u3014778@users.noreply.github.com>
2024-11-24 23:34:44 +1100

    set supabaseKey

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,7 +4,8 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 // Supabase client initialization
 //const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseUrl = 'https://juxdmkjcmbgerbybwmer.supabase.co';
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+//const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp1eGRta2pjbWJnZXJieWJ3bWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE3MjgwMDUsImV4cCI6MjAzNzMwNDAwNX0.9o_XvQfoSEFKj3TnQCIXvMYl02s_q90TYnU8BpjFOdg';
 const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
